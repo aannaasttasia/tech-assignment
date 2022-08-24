32190d3932699e5e3bd4c415242bf4c8a2fc62ab
aannaasttasia <nastyahavruliyk@gmail.com>
2022-08-24 10:41:36 +0300

    the third commit

--- src/components/Indexes/index.tsx
+++ src/components/Indexes/index.tsx
@@ -1,39 +1,41 @@
-import React from 'react';
-import { Component } from 'react';
-import { Index } from '../../constants/types';
-import './css/Indexes.scss';
+import React from "react";
+import { Component } from "react";
+import { Index } from "../../constants/types";
+import './css/Indexes.scss'
 
 interface IndexesProps {
-    indexes: Index[]
+  indexes: Index[];
 }
 
-export default class Indexes extends Component<IndexesProps>{
-    constructor(props: IndexesProps){
-        super(props);
-    }
+export default class Indexes extends Component<IndexesProps> {
+  constructor(props: IndexesProps) {
+    super(props);
+  }
 
-    renderIndexes = (indexes: Index[]) => {
-        return (
-        <div className='blockParent'>
-            { indexes.map((a,i)=> {
-            return(
-                <div  className="block" key={i}>
-                    <p className='name'>{a.name}</p>
-                    <p>${(a.usdPriceInCents).toString()}/{(a.ethPriceInWei).toString()} ETH</p>
-                    <div className='inlineEl'>
-                        <p>${(a.usdCapitalization).toString()}</p>
-                        <p className='percentageChange'>{(a.percentageChange).toString()}%</p>
-                    </div>
-                    
-                </div>
-            )
+  renderIndexes = (indexes: Index[]) => {
+    return (
+      <div className="blockParent">
+        {indexes.map((a, i) => {
+          return (
+            <div className="block" key={i}>
+              <p className="name">{a.name}</p>
+              <p>
+                ${a.usdPriceInCents.toString()}/{a.ethPriceInWei.toString()} ETH
+              </p>
+              <div className="inlineEl">
+                <p>${a.usdCapitalization.toString()}</p>
+                <p className="percentageChange">
+                  {a.percentageChange.toString()}%
+                </p>
+              </div>
+            </div>
+          );
         })}
-        </div>)
+      </div>
+    );
   };
 
-    render() {
-        return(
-            (this.renderIndexes(this.props.indexes))
-        )
-    }
+  render() {
+    return this.renderIndexes(this.props.indexes);
+  }
 }
--- src/components/Name/index.tsx
+++ src/components/Name/index.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import './css/Name.scss'
+
+
 export default class Name extends Component<{ name: string }> {
 
   render() {
