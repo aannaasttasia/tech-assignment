42614440c49fbb3fd486310e8740ed3ac40a892d
aannaasttasia <nastyahavruliyk@gmail.com>
2022-08-30 13:40:54 +0300

    Change currency format

--- src/components/Indexes/index.tsx
+++ src/components/Indexes/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Component } from "react";
 import { Index } from "../../constants/types";
 import './css/Indexes.scss'
-
+import * as convert from 'ether-converter';
 interface IndexesProps {
   indexes: Index[];
 }
@@ -19,11 +19,19 @@ export default class Indexes extends Component<IndexesProps> {
           return (
             <div className="block" key={i}>
               <p className="name">{a.name}</p>
-              <p>
-                ${a.usdPriceInCents.toString()}/{a.ethPriceInWei.toString()} ETH
-              </p>
+              <p>{(a.usdPriceInCents / 100).toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              })}/{convert(a.ethPriceInWei.toString(), 'wei', 'ether')} ETH</p>
               <div className="inlineEl">
-                <p>${a.usdCapitalization.toString()}</p>
+                <p>{(a.usdCapitalization/100).toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}</p>
                 <p className="percentageChange">
                   {a.percentageChange.toString()}%
                 </p>
